677d258b986899cfed1ac139808e7dc23bc443cf
ecf4CDA <ecf@4cda.com>
2023-06-06 22:01:05 +0800

    Added historic injury data scraping function

--- run_afl_injury_web_scraper.py
+++ run_afl_injury_web_scraper.py
@@ -1,7 +1,7 @@
 from web_scraper_functions import *
 
 keys = get_keys()
-df = get_latest_injuries_df()
+df = get_latest_injuries_df("https://www.afl.com.au/matches/injury-list")
 upload_data_to_db(
     df,
     db_host=keys['db_host'],
